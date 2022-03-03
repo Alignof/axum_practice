92076818e51b5b8a0278a1e3cb941dbf37ddc249
Alingof <Alignof@outlook.com>
2022-03-03 19:43:58 +0900

    [update] change the function_component to impl Component

--- frontend/src/components/home.rs
+++ frontend/src/components/home.rs
@@ -1,19 +1,50 @@
-use yew::{function_component, html};
-use crate::Msg;
+use yew::{html, Component, Context, Html};
 
-#[function_component(Home)]
-pub fn home() -> Html {
-    // style for tailwind
-    let btn_sty = "bg-white text-blue-800 m-3 rounded py-2 px-4";
-
-    html! {
-        <div class="m-5">
-            <button class={btn_sty} onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-            <button class={btn_sty} onclick={link.callback(|_| Msg::MinusOne)}>{ "-1" }</button>
-            <p class="text-3xl">{ self.value }</p>
-        </div>
-    }
+pub enum Msg {
+    AddOne,
+    MinusOne,
 }
 
+pub struct Home {
+    value: i64,
+}
 
+impl Component for Home {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            },
+            Msg::MinusOne => {
+                self.value -= 1;
+                true
+            },
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        // style for tailwind
+        let btn_sty = "bg-white text-blue-800 m-3 rounded py-2 px-4";
+
+        html! {
+            <div class="m-5">
+                <button class={btn_sty} onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <button class={btn_sty} onclick={link.callback(|_| Msg::MinusOne)}>{ "-1" }</button>
+                <p class="text-3xl">{ self.value }</p>
+            </div>
+        }
+    }
+}
 
